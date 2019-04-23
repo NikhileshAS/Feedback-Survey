969853683b8fa17b4c8db27cf5caee04d0e05b3b
NikhileshAS <32016285+NikhileshAS@users.noreply.github.com>
2019-04-23 22:58:29 +0530

    Typo Authentiation

--- configs/prod.js
+++ configs/prod.js
@@ -1,7 +1,7 @@
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID,
-const GOOGLE_SECRET_KEY = process.env.GOOGLE_SECRET_KEY,
-const MONGODB_URI =process.env.MONGODB_URI,
-const COOKIE_KEY = process.env.COOKIE_KEY
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_SECRET_KEY = process.env.GOOGLE_SECRET_KEY;
+const MONGODB_URI = process.env.MONGODB_URI;
+const COOKIE_KEY = process.env.COOKIE_KEY;
 
 module.exports = {
   GOOGLE_CLIENT_ID,
